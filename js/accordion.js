07ebf472c82498b0965234b4a56ef91968c8c84d
ayo idan <ayo_idan@outlook.com>
2022-09-12 10:14:04 +0100

    Fixed doi links

--- js/accordion.js
+++ js/accordion.js
@@ -1,35 +1,31 @@
-
-$(function() {
+$(function () {
   // Initialise the accordion:
-  $('#accordion').accordion({
-    heightStyle: 'content',
+  $("#accordion").accordion({
+    heightStyle: "content",
     collapsible: true,
     active: false,
-    header: 'h1'
+    header: "h1",
   });
   // Get the #hash in the URL:
   var hash = window.location.hash;
   hash = hash.slice(1);
-  console.log('boop');
+  console.log("boop");
   // console.log(hash);
   var active = false;
   // Activate appropriate accordion item and scroll to it:
-  if (hash === 'talks') {
+  if (hash === "talks") {
     active = 0;
     console.log(0);
-
-  } else if (hash.slice(0,3) === 'pub') {
+  } else if (hash.slice(0, 3) === "pub") {
     active = 1;
-  } else if (hash === 'pub2') {
+  } else if (hash === "pub2") {
     active = 2;
-  } 
-
-
-  if (hash) {
-    $('#the_accordion').accordion('option', 'active', active);
-    $('html,body').animate({
-      scrollTop: $('#the_accordion').offset().top
-    });
   }
 
+  if (hash) {
+    $("#the_accordion").accordion("option", "active", active);
+    $("html,body").animate({
+      scrollTop: $("#the_accordion").offset().top,
+    });
+  }
 });
